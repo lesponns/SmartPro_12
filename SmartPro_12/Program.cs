178cb08d1322c75a49bb1cfd766f07fb76089945
lesponns <adamadakadam1903@gmail.com>
2023-07-27 19:26:47 +0300

    Add project files.

---
+++ SmartPro_12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baslangic, bitis;
+
+            Console.Write("Başlamak istediğniz sayı: ");
+            baslangic = int.Parse(Console.ReadLine());
+
+            Console.Write("Bitirmek istediğiniz sayı: ");
+            bitis = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
